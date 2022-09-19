e755c3728922b9360e37082d65c0873c7d3f30c8
Gaspard Corentin <corentin_gaspard@ens.univ-artois.fr>
2022-09-19 15:30:58 +0200

    bug merge

--- Projet-Site-Appli/src/app/app.component.ts
+++ Projet-Site-Appli/src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent {
   title = 'Projet-Site-Appli';
   nb_joueur = 0;
 
-  
-  
+
+
 
   constructor(private translate: TranslateService, private modalService: NgbModal) {
     translate.addLangs(['fr', 'en']);
@@ -22,19 +22,6 @@ export class AppComponent {
   }
 
   public isCollapsed = false;
-<<<<<<< Projet-Site-Appli/src/app/app.component.ts
-
-  tab(): void {
-    var tab_create = false;
-    var tabSurvie = new Array(this.nb_joueur);
-    if(this.nb_joueur >= 2 && this.nb_joueur <=10){
-      for(var i=1;i<tabSurvie.length;i++){
-        tabSurvie.push(i);
-        tab_create = true;
-    }
-    }
-    
-=======
   selectedLang: any;
 
   open(content: any) {
@@ -45,6 +32,5 @@ export class AppComponent {
     this.selectedLang = deviceValue;
     console.log(this.selectedLang)
     this.translate.use(deviceValue);
->>>>>>> Projet-Site-Appli/src/app/app.component.ts
   }
 }
