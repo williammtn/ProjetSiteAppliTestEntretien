86a08f0dfcf4a3b3f8785928993226ce848dfc5d
FunkyGlitch <corentin_gaspard@ens.univ-artois.fr>
2022-09-29 17:12:40 +0200

    Random question + affichage num question

--- Projet-Site-Appli/src/app/survival/survival.component.ts
+++ Projet-Site-Appli/src/app/survival/survival.component.ts
@@ -4,6 +4,7 @@ import {NgbAlert, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import { Categories } from '../interfaces/Categories';
 import { HttpClient } from '@angular/common/http';
 import { Questions } from '../interfaces/Questions';
+import {QuestionService} from "../service/question.service";
 
 @Component({
   selector: 'app-survival',
@@ -37,8 +38,9 @@ export class SurvivalComponent implements OnInit {
       },1000)
     }
   }
+/*
 
-  getQuestions(): Observable<any> {
+ getQuestions(): Observable<any> {
     var url = 'http://45.155.170.233:3000/questions?survival_mode=eq.true';
     let tab: any[];
     tab = [];
@@ -48,12 +50,11 @@ export class SurvivalComponent implements OnInit {
     });
     console.log(tab);
     return  this.http.get(url);
-
   }
+*/
 
-
-  constructor(private modalService: NgbModal, private http: HttpClient) {
-    this.getQuestions().subscribe(req => this.questions = req);
+  constructor(private modalService: NgbModal, private http: HttpClient,private questionService: QuestionService) {
+    this.questionService.getQuestionsSurvival().subscribe(res => this.questions = res);
   }
 
   ngOnInit(): void {
@@ -65,7 +66,7 @@ export class SurvivalComponent implements OnInit {
     //nb_joueur =document.getElementById("nbj");
     //var tabSurvie = new Array(nb_joueur);
     if(nb_joueur >= 2 && nb_joueur <=10){
-        this.tab_create = true;
+      this.tab_create = true;
     }
   }
 
@@ -75,7 +76,25 @@ export class SurvivalComponent implements OnInit {
   }
 
   errorMessage = '';
-
+  bool: boolean = true;
+  question : number =  Math.floor(Math.random() * 20);
+  IdQuestion : number = 1;
+  tabQ : number[] = [this.question];
+  incIdQuestion(){
+    return this.IdQuestion++;
+  }
+  IncQuestion(){
+    let r =  Math.floor((Math.random() * 20));
+    while(true) {
+      if (!this.tabQ.includes(r)) {
+        this.tabQ.push(r);
+        this.incIdQuestion();
+        return this.question = r;
+      }
+      r =  Math.floor((Math.random() * 20));
+    }
+    return 0;
+  }
   envoyer()  {
     this.joueurs = this.model;
     if(this.joueurs >= 2 && this.joueurs <= 10) {
@@ -91,3 +110,4 @@ export class SurvivalComponent implements OnInit {
     return true;
   }
 }
+
